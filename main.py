cd4985ef04c51519917134d9959be22e7ce25810
mr.kustik1985 <mr.kustik1985@gmail.com>
2023-03-27 23:42:44 +0300

    add buttom

--- main.py
+++ main.py
@@ -21,41 +21,41 @@ class TextEditor:
         self.last_action.set("welcome to text editor")
 
         # create main menu
-        self.main_menu = Menu(self.root, font=("times new roman",12,"bold"))
+        self.main_menu = Menu(self.root, font=("times new roman",12,"bold"), activebackground="skyblue")
         self.root.config(menu=self.main_menu)
 
         # create menu to work with file
-        self.file_menu = Menu(self.main_menu, font=("times new roman",12,"bold"), tearoff=0)
+        self.file_menu = Menu(self.main_menu, font=("times new roman",12,"bold"), activebackground="skyblue", tearoff=0)
         self.file_menu.add_command(label="New", accelerator="Ctrl+N", command=self.new_file)
         self.file_menu.add_command(label="Open", accelerator="Ctrl+O", command=self.open_file)
-        self.file_menu.add_command(label="Save", accelerator="Ctrl+S")
-        self.file_menu.add_command(label="Save as", accelerator="Ctrl+S")
+        self.file_menu.add_command(label="Save", accelerator="Ctrl+S", command = self.save_file)
+        self.file_menu.add_command(label="Save as", accelerator="Ctrl+A", command=self.first_save_file)
         self.file_menu.add_separator()
         self.file_menu.add_command(label="Exit",accelerator="Ctrl+E",command=self.exit)
 
         # create menu to work with edit (to do function for hot keys)
-        self.edit_menu = Menu(self.main_menu, font=("times new roman",12,"bold"), tearoff=0)
+        self.edit_menu = Menu(self.main_menu, font=("times new roman",12,"bold"), activebackground="skyblue", tearoff=0)
         # adding cut text command
-        self.edit_menu.add_command(label="Cut",accelerator="Ctrl+X")
+        self.edit_menu.add_command(label="Cut",accelerator="Ctrl+X", command=self.cut)
         # adding copy text command
-        self.edit_menu.add_command(label="Copy",accelerator="Ctrl+C")
+        self.edit_menu.add_command(label="Copy",accelerator="Ctrl+C", command=self.copy)
         # adding paste text command
-        self.edit_menu.add_command(label="Paste",accelerator="Ctrl+V")
+        self.edit_menu.add_command(label="Paste",accelerator="Ctrl+V", command=self.paste)
+        self.edit_menu.add_command(label="Cancel",accelerator="Ctrl+Z", command=self.cancel_action)
         # adding seprator
         self.edit_menu.add_separator()
         # adding undo text Command
-        self.edit_menu.add_command(label="Undo",accelerator="Ctrl+U")
+        self.edit_menu.add_command(label="Undo",accelerator="Ctrl+U", command=self.undo)
 
         # creating help menu
 
-        self.help_menu = Menu(self.main_menu, font=("times new roman",12,"bold"), tearoff=0)
+        self.help_menu = Menu(self.main_menu, font=("times new roman",12,"bold"), activebackground="skyblue", tearoff=0)
         self.help_menu.add_command(label="About")
 
         self.main_menu.add_cascade(label="File",menu=self.file_menu)
         self.main_menu.add_cascade(label="Edit",menu=self.edit_menu)
         self.main_menu.add_cascade(label="Help",menu=self.help_menu)
 
-        self.scrollbar = Scrollbar(self.root)
         self.text = Text(self.root,
                         font=("times new roman",15,"bold"), 
                         bd = 2,
@@ -65,13 +65,20 @@ class TextEditor:
                         pady = 10, 
                         wrap = WORD,
                         insertbackground = "white",
-                        yscrollcommand=self.scrollbar.set
+                        undo=True
                         )
-        self.scrollbar.config(command=self.text.yview)
+        self.scrollbar = Scrollbar(orient="vertical", command= self.text.yview)
+        self.text["yscrollcommand"] = self.scrollbar.set
         self.scrollbar.pack(side=RIGHT,fill=Y)
         self.text.pack(fill=BOTH,expand=1)
         self.add_hot_key()
 
+
+    def undo(self, event=None):
+        if self.steps != 0:
+            self.steps -= 1
+            self.delete(0, END)
+            self.insert(END, self.changes[self.steps])
     # print file name TOP page
     def is_open_file(self):
         if self.filename!= None:
@@ -103,7 +110,26 @@ class TextEditor:
                 self.last_action.set("file opened")
         except Exception as e:
             messagebox.showerror("ERROR", e)
+    
+    def save_file(self, *args):
+        try:
+            if self.filename!= None:
+                with open(self.filename, "w") as f:
+                    f.write(self.text.get(1.0,END))
+                self.last_action.set("file saved")
+            else:
+                op = messagebox.askyesno("your file is unnamed","save it?")
+                if op>0:
+                    self.first_save_file()
+        except Exception as e:
+            messagebox.showerror("ERROR", e)
 
+    def first_save_file(self, *args):
+        self.filename = fd.asksaveasfilename(filetypes = (("All Files","*.*"),("Text Files","*.txt"),("Python Files","*.py")))
+        self.is_open_file()
+        self.last_action.set("file overwrite")
+
+    # to do it than file already saved didn't do over work
     def exit(self, *args):
         op = messagebox.askyesno("WARNING","Your Unsaved Data May be Lost!!")
         if op>0:
@@ -111,10 +137,36 @@ class TextEditor:
         else:
             return
         
+    def cut(self, *args):
+        self.text.event_generate("<<Cut>>")
+
+    def copy(self, *args):
+        self.text.event_generate("<<Copy>>")
+
+    def paste(self, *args):
+        self.text.event_generate("<<Paste>>")
+
+    def undo(self, *args):
+        try:
+            self.text.delete(1.0,END)
+            self.status.set("Undone Successfully")
+        except Exception as e:
+            messagebox.showerror("Exception",e)
+    
+    def cancel_action(self, *args):
+        self.text.edit_undo()
+    
     def add_hot_key(self):
         self.text.bind("<Control-e>",self.exit)
         self.text.bind("<Control-n>",self.new_file)
         self.text.bind("<Control-o>",self.open_file)
+        self.text.bind("<Control-s>",self.save_file)
+        self.text.bind("<Control-s>",self.first_save_file)
+        self.text.bind("<Control-x>",self.cut)
+        self.text.bind("<Control-c>",self.copy)
+        self.text.bind("<Control-v>",self.paste)
+        self.text.bind("<Control-u>",self.undo)
+        self.text.bind("<Control-z>",self.cancel_action)
 
 root = Tk()
 TextEditor(root)
